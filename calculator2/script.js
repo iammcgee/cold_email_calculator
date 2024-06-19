a04e354f4a827f8eef39c005ac270eb28e960eba
iammcgee <72765709+iammcgee@users.noreply.github.com>
2024-06-19 13:57:05 -0500

    Update script.js

--- calculator2/script.js
+++ calculator2/script.js
@@ -1,7 +1,8 @@
 function calculate() {
     const targetEmails = document.getElementById('target_emails').value;
     const totalLeads = document.getElementById('total_leads').value;
-    const riskPercentage = document.getElementById('risk_percentage').value;
+    const identifiedEmails = document.getElementById('identified_emails').value;
+    const usableEmails = document.getElementById('usable_emails').value;
     const resultList = document.getElementById('resultList');
     const resultDiv = document.getElementById('result');
     const errorDiv = document.getElementById('error');
@@ -10,18 +11,23 @@ function calculate() {
     resultDiv.style.display = 'none';
     errorDiv.innerHTML = '';
 
-    if (targetEmails) {
-        const totalCreditsNeeded = Math.ceil((targetEmails / (1 - (riskPercentage / 100))) / 1000) * 1000;
+    let percentageUsableEmails = 0;
+
+    if (identifiedEmails && usableEmails) {
+        percentageUsableEmails = (usableEmails / identifiedEmails) * 100;
+    }
+
+    if (targetEmails && percentageUsableEmails) {
+        const leadsNeeded = Math.ceil((targetEmails / (percentageUsableEmails / 100)));
+        resultList.innerHTML += `<li>Number of Leads to Purchase: ${leadsNeeded}</li>`;
+    } else if (totalLeads && identifiedEmails && usableEmails) {
+        const riskPercentage = 100 - percentageUsableEmails;
+        const totalCreditsNeeded = Math.ceil((totalLeads * (riskPercentage / 100)) / 1000) * 1000;
         resultList.innerHTML += `<li>Total Credits Needed: ${totalCreditsNeeded}</li>`;
-    } else if (totalLeads) {
-        const riskLeads = totalLeads * (riskPercentage / 100);
-        const usableEmails = totalLeads - riskLeads;
-        resultList.innerHTML += `<li>Usable Emails: ${Math.floor(usableEmails)}</li>`;
-    } else if (riskPercentage) {
-        errorDiv.innerHTML = 'Please provide either target emails or total leads.';
-        return;
+    } else if (identifiedEmails && usableEmails) {
+        resultList.innerHTML += `<li>Percentage of Usable Emails: ${percentageUsableEmails.toFixed(2)}%</li>`;
     } else {
-        errorDiv.innerHTML = 'Please provide at least one input value.';
+        errorDiv.innerHTML = 'Please provide the necessary input values.';
         return;
     }
 
@@ -31,7 +37,8 @@ function calculate() {
 function clearInputs() {
     document.getElementById('target_emails').value = '';
     document.getElementById('total_leads').value = '';
-    document.getElementById('risk_percentage').value = '';
+    document.getElementById('identified_emails').value = '';
+    document.getElementById('usable_emails').value = '';
     document.getElementById('resultList').innerHTML = '';
     document.getElementById('result').style.display = 'none';
     document.getElementById('error').innerHTML = '';
